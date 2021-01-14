4267bd9fc50528f2493672bea48e56b3a57170b2
Volha <kresiko@gmail.com>
2021-01-14 17:46:30 +0200

    Level2 lesson 1 improved. Interfaces

---
+++ org.volha.geekbrains.java.level2/src/main/java/lesson1improved/lesson1/Jumpable.java
@@ -0,0 +1,5 @@
+package lesson1improved.lesson1;
+
+public interface Jumpable {
+    boolean jump(int jumpHeght);
+}
---
+++ org.volha.geekbrains.java.level2/src/main/java/lesson1improved/lesson1/Robot.java
@@ -0,0 +1,49 @@
+package lesson1improved.lesson1;
+
+public class Robot extends Entity {
+    public Robot(String name, int maxRun, int maxJump) {
+        super(name, maxRun, maxJump);
+    }
+}
+//implements Runable, Jumpable{
+//    String name;
+//    int maxRun;
+//    int maxJump;
+//
+//    public Human(String name, int maxRun, int maxJump) {
+//        this.name = name;
+//        this.maxRun = maxRun;
+//        this.maxJump = maxJump;
+//    }
+//
+//    void info(){
+//        System.out.println("Human " + name + " can run " +maxRun + " can jump " + maxJump);
+//    }
+//
+//    @Override
+//    public void run( int runDistance){
+//        if (runDistance <= this.maxRun) {
+//            System.out.println("Human " + name + " successfully ran " + runDistance);
+//        } else {
+//            System.out.println("Human " + name + " can't run " + runDistance + " it can only " + maxRun);
+//        }
+//    }
+//
+//    @Override
+//    public void jump( int jumpHeight){
+//        if (jumpHeight <= this.maxJump) {
+//            System.out.println("Human " + name + " successfully jumped over " + jumpHeight);
+//        } else {
+//            System.out.println("Human " + name + " can't jump over " + jumpHeight + " it can only " + maxJump);
+//        }
+//    }
+////    @Override
+////    public void run() {
+////        System.out.println("Robot ran successfully");
+////    }
+////
+////    @Override
+////    public void jump() {
+////        System.out.println("Robot jumped successfully");
+////    }
+//}
