ef238a903fa39f0a826bd30ee6afc95f9be8c00e
Volha <kresiko@gmail.com>
2021-03-10 07:50:32 +0200

    Threads II hw. Race

--- org.volha.geekbrains.java.level3/threadracefromexample/src/main/java/race/Finish.java
+++ org.volha.geekbrains.java.level3/threadracefromexample/src/main/java/race/Finish.java
@@ -1,9 +1,10 @@
 package race;
 
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Finish extends Stage {
-    volatile boolean winnner = false;
+    AtomicBoolean winner = new AtomicBoolean(false);
 
     public Finish() {
         this.description = "Finish";
@@ -13,8 +14,8 @@ public class Finish extends Stage {
     public void go(Car c) {
         System.out.println(c.getName() + " начал этап: " + description);
         System.out.println(c.getName() + " закончил этап: " + description);
-        if (!winnner) {
-            winnner = true;
+        if (!winner.get()) {
+            winner.set(true);
             System.out.println(c.getName() + " WON ");
                     }
     }
